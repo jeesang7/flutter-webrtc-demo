cd1d0b37125183f3d4ab32c6cbb784d6a38ba748
Jeesang Kim <jeenowden@gmail.com>
2021-11-26 15:43:38 +0900

    replace main page and startup two buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,73 @@
-import 'dart:core';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'src/call_sample/call_sample.dart';
-import 'src/call_sample/data_channel_sample.dart';
-import 'src/route_item.dart';
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-enum DialogDemoAction {
-  cancel,
-  connect,
-}
-
-class _MyAppState extends State<MyApp> {
-  List<RouteItem> items = [];
-  String _server = '';
-  late SharedPreferences _prefs;
-
-  bool _datachannel = false;
-  @override
-  initState() {
-    super.initState();
-    _initData();
-    _initItems();
-  }
-
-  _buildRow(context, item) {
-    return ListBody(children: <Widget>[
-      ListTile(
-        title: Text(item.title),
-        onTap: () => item.push(context),
-        trailing: Icon(Icons.arrow_right),
-      ),
-      Divider()
-    ]);
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter-WebRTC example'),
-          ),
-          body: ListView.builder(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(0.0),
-              itemCount: items.length,
-              itemBuilder: (context, i) {
-                return _buildRow(context, items[i]);
-              })),
+      title: 'CAREBOX',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
     );
   }
+}
 
-  _initData() async {
-    _prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _server = _prefs.getString('server') ?? 'demo.cloudwebrtc.com';
-    });
-  }
+class MyHomePage extends StatefulWidget {
+  MyHomePage();
 
-  void showDemoDialog<T>(
-      {required BuildContext context, required Widget child}) {
-    showDialog<T>(
-      context: context,
-      builder: (BuildContext context) => child,
-    ).then<void>((T? value) {
-      // The value passed to Navigator.pop() or null.
-      if (value != null) {
-        if (value == DialogDemoAction.connect) {
-          _prefs.setString('server', _server);
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (BuildContext context) => _datachannel
-                      ? DataChannelSample(host: _server)
-                      : CallSample(host: _server)));
-        }
-      }
-    });
-  }
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  _showAddressDialog(context) {
-    showDemoDialog<DialogDemoAction>(
-        context: context,
-        child: AlertDialog(
-            title: const Text('Enter server address:'),
-            content: TextField(
-              onChanged: (String text) {
-                setState(() {
-                  _server = text;
-                });
-              },
-              decoration: InputDecoration(
-                hintText: _server,
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("케어박스 CAREBOX"),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ButtonTheme(
+              minWidth: size.width * 0.3,
+              height: size.height * 0.7,
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  "통화하기",
+                  style: Theme.of(context).textTheme.headline3!.copyWith(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
               ),
-              textAlign: TextAlign.center,
             ),
-            actions: <Widget>[
-              FlatButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    Navigator.pop(context, DialogDemoAction.cancel);
-                  }),
-              FlatButton(
-                  child: const Text('CONNECT'),
-                  onPressed: () {
-                    Navigator.pop(context, DialogDemoAction.connect);
-                  })
-            ]));
-  }
-
-  _initItems() {
-    items = <RouteItem>[
-      RouteItem(
-          title: 'P2P Call Sample',
-          subtitle: 'P2P Call Sample.',
-          push: (BuildContext context) {
-            _datachannel = false;
-            _showAddressDialog(context);
-          }),
-      RouteItem(
-          title: 'Data Channel Sample',
-          subtitle: 'P2P Data Channel.',
-          push: (BuildContext context) {
-            _datachannel = true;
-            _showAddressDialog(context);
-          }),
-    ];
+            SizedBox(
+              width: size.width * 0.15,
+            ),
+            ButtonTheme(
+              minWidth: size.width * 0.3,
+              height: size.height * 0.7,
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  "관리자",
+                  style: Theme.of(context).textTheme.headline3!.copyWith(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
